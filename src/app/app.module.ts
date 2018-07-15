a734c4eb1ea03563a95e7ac771ec28bacf7ee53e
Sagarmoy Sengupta <sagarmoysengupta@gmail.com>
2018-07-15 10:39:25 +0530

    First Layout Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './_layout/header/header.component';
+import { BodyComponent } from './_layout/body/body.component';
+import { FooterComponent } from './_layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
